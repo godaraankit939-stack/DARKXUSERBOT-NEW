73d83355322072957ae01192a74410eda0bd4d3b
godaraankit939-stack <godaraankit939@gmail.com>
2026-03-16 18:32:02 +0530

    Update animation.py

--- plugins/animation.py
+++ plugins/animation.py
@@ -1,7 +1,1 @@
-from telethon import events
-import asyncio
 
-@client.on(events.NewMessage(outgoing=True, pattern=r"^\.test$"))
-async def test_handler(event):
-    await event.edit("✅ Animation Plugin Loaded & Working!")
-    
